a45994c24ac2b7e5d2c51373320918ae60c75364
ashiqurrhmn <arakash115@gmail.com>
2026-04-06 00:28:58 +0600

    setup

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout/Mainlayout";
+import Books from "../pages/Books/Books";
+import Homepage from "../pages/Homepage/Homepage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout/>,
+    children: [
+      {
+        index: true,
+        element: <Homepage/>,
+      },
+      {
+        path: "/books",
+        element: <Books/>,
+      },
+    ]
+  },
+ 
+]);
